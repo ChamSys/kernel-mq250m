521304cc09014e614821efddd93933a720fc9a51
James Crickmere <james.crickmere@chamsys.co.uk>
2021-06-29 17:49:16 +0100

    [ChamSys Ltd] Add boot logo on MQ250M (optimised for 1920x1080, matches BIOS)

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -100,6 +100,10 @@ const struct linux_logo * __ref fb_find_logo(int depth)
 		/* SuperH Linux logo */
 		logo = &logo_superh_clut224;
 #endif
+#ifdef CONFIG_LOGO_CHAMSYS_CLUT224
+		/* ChamSys logo for 1920x1080 */
+		logo = &logo_chamsys_clut224;
+#endif
 	}
 	return logo;
 }
